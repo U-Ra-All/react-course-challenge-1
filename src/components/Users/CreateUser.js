c63d748fa5a73c885af0a821299c1256402fa08a
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-30 11:15:39 +0200

    03-Добавление Компонента Card

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,16 +1,22 @@
+import Card from "../UI/Card";
+
+import styles from "./CreateUser.module.css";
+
 const CreateUser = (props) => {
   const createUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={createUserHandler}>
-      <label htmlFor="name">Имя</label>
-      <input id="name" type="text" />
-      <label htmlFor="age">Возраст</label>
-      <input id="age" type="number" />
-      <button type="submit">Добавить Пользователя</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={createUserHandler}>
+        <label htmlFor="name">Имя</label>
+        <input id="name" type="text" />
+        <label htmlFor="age">Возраст</label>
+        <input id="age" type="number" />
+        <button type="submit">Добавить Пользователя</button>
+      </form>
+    </Card>
   );
 };
 
